be277ba10a463f13efed1ea3c3db6f3d9492f13a
Raffaele Sena <raff367@gmail.com>
2014-03-25 20:46:24 -0700

    Added import of "tuple" and definition of "error"

--- runtime/c/go.h
+++ runtime/c/go.h
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <tuple>
 
 // for string, map, etc.
 using namespace std;
 
 typedef unsigned char byte;
+
+typedef string error;
 
 inline void panic(string &arg) {
     cerr << "panic: " << arg << endl;
