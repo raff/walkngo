47cc609777f389dd12c197de23c881e5740464f0
Raffaele Sena <raff367@gmail.com>
2014-03-26 14:10:55 -0700

    Added typedefs for Go numeric types

--- runtime/c/go.h
+++ runtime/c/go.h
@@ -6,7 +6,21 @@
 // for string, map, etc.
 using namespace std;
 
-typedef unsigned char byte;
+typedef unsigned char      uint8;
+typedef unsigned short int uint16;
+typedef unsigned int       uint32;
+typedef unsigned long int  uint64;
+
+typedef signed char      int8;
+typedef signed short int int16;
+typedef signed int       int32;
+typedef signed long int  int64;
+
+typedef float  float32;
+typedef double float64;
+
+typedef uint8 byte;
+typedef int32 rune;
 
 typedef string error;
 
